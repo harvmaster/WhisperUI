387d5b57007803c6f8547bd0f4fdd0e811bedd49
Harvmaster <hzuccon@gmail.com>
2024-05-17 01:50:25 +1000

    Added animation to hide and show transcript

--- src/components/AudioFile/AudioFile.vue
+++ src/components/AudioFile/AudioFile.vue
@@ -2,21 +2,28 @@
   <div class="">
     <div class="audio-file-container row" :style="containerStyle">
 
-      <!-- Header -->
-      <div ref="audioFileHeader" class="audio-file-header q-pa-md pill col-12 row">
-        <div class="col-auto q-pr-md">
-          <q-btn flat round dense icon="play_arrow" color="blue-4" @click="playAudio" />
+      <!-- file name -->
+      <span class="col-12 fit-height relative text-h6 q-px-md row" :style="fileNameHeight">
+        <span class="col-12 self-center">
+          My File Name
+        </span>
+        <!-- Header -->
+        <div ref="audioFileHeader" class="audio-file-header q-pa-md pill col-12 row" :style="`transform: translateY(${expanded ? '0' : '100%'});`">
+          <div class="col-auto q-pr-md">
+            <q-btn flat round dense icon="play_arrow" color="blue-4" @click="playAudio" />
+          </div>
+  
+          <div class="col row">
+            <audio-track class="col-12" />
+          </div>
+  
+          <div class="col-auto">
+            <q-btn v-if="!expanded" flat round dense icon="keyboard_arrow_down" color="blue-4" @click="expand" />
+            <q-btn v-else flat round dense icon="keyboard_arrow_up" color="blue-4" @click="collapse" />
+          </div>
         </div>
+      </span>
 
-        <div class="col row">
-          <audio-track class="col-12" />
-        </div>
-
-        <div class="col-auto">
-          <q-btn v-if="!expanded" flat round dense icon="keyboard_arrow_down" color="blue-4" @click="expand" />
-          <q-btn v-else flat round dense icon="keyboard_arrow_up" color="blue-4" @click="collapse" />
-        </div>
-      </div>
 
       <!-- Body -->
       <transition name="shrink-y">
@@ -58,6 +65,14 @@
 }
 
 .audio-file-header {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  transform: translateY(100%);
+
+  transition: transform 0.5s;
+
   background-color: $blue-2;
   box-shadow: 0 0.1rem 1rem rgba(0, 0, 0, 0.1);
 }
@@ -87,11 +102,20 @@ import AudioTrack from './AudioTrack.vue'
 
 const audioFileHeader = ref<HTMLElement | null>(null)
 
-const containerStyle = computed(() => {
-  const headerHeight = audioFileHeader.value ? audioFileHeader.value.clientHeight : 0
-  
+const headerHeight = computed(() => {
+  return audioFileHeader.value ? audioFileHeader.value.clientHeight : 0
+})
+
+const fileNameHeight = computed(() => {
   return {
-    borderRadius: `${headerHeight/2}px`
+    height: `${headerHeight.value}px`
+  }
+})
+
+const containerStyle = computed(() => {
+  return {
+    borderRadius: `${headerHeight.value/2}px`,
+    minHeight: `${headerHeight.value*2}px`
   }
 })
 
