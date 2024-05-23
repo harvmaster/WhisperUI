1d9c7ab1ec1007979a06a5fb84f671ad2600c0a3
Harvmaster <hzuccon@gmail.com>
2024-05-23 23:00:10 +1000

    Added numToLocaeTime fnction

---
+++ src/lib/Time/index.ts
@@ -0,0 +1,1 @@
+export { default as numToLocaleTime } from './numToLocaleTime';
---
+++ src/lib/Time/numToLocaleTime.ts
@@ -0,0 +1,12 @@
+export const numToLocaleTime = (num: number): string => {
+  const seconds = num % 60;
+  const minutes = Math.floor(num / 60) % 60;
+  const hours = Math.floor(num / 3600);
+
+  if (hours > 0) return `${hours}:${minutes}:${seconds}`;
+  if (minutes > 0) return `${minutes}:${seconds}`;
+
+  return `00:${seconds}`;
+}
+
+export default numToLocaleTime;
