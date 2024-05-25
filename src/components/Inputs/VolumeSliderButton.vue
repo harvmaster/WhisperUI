5bfbf4ee582040ec51bdfa6cf1d26554370184e9
Harvmaster <hzuccon@gmail.com>
2024-05-25 17:55:13 +1000

    Basic volume slider component

---
+++ src/components/Inputs/VolumeSliderButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <control-button
+    :icon="props.icon"
+    @click="props.onClick"
+
+  >
+    <template v-slot:after>
+      <q-slider v-model="props.value" class="volume-slider" :class="props.showVolume ? 'active-slider' : ''"  :min="0" :max="1" :step="0.01" />
+    </template>
+  </control-button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script setup lang="ts">
+import controlButton from 'src/components/Inputs/ControlButton.vue'
+
+
+</script>
